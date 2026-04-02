e8939619b437bdffeb56ea0649ce00af0d0282db
andreavrob <andreavrobledo97@gmail.com>
2026-04-01 21:50:09 -0600

    Hello Aura

---
+++ main.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+st.title("🎨 Aura Art Scanner")
+st.caption("This is a tool that helps you scan art and get information about it  🖌")
+
+prompt = st.chat_input(
+    "Say something and/or attach an image",
+    accept_file=True,
+    file_type=["jpg", "jpeg", "png"],
+)
+if prompt and prompt.text:
+    st.markdown(prompt.text)
+if prompt and prompt["files"]:
+    st.image(prompt["files"][0])
